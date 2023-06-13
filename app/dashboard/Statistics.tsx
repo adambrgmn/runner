9b2eb842c98b18bf7eec9d3dc335f4de280a361f
Adam Bergman <adam@fransvilhelm.com>
2023-06-13 14:33:53 +0200

    Create spinner fallback

--- app/dashboard/Statistics.tsx
+++ app/dashboard/Statistics.tsx
@@ -26,11 +26,5 @@ export async function Statistics({ now }: StatisticsProps) {
         } as const),
     );
 
-  await sleep(10_000);
-
   return <StatisticsTable data={data} />;
-}
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
+import { Spinner } from '@/components/Spinner';
 import { getGoal } from '@/lib/goal';
 
 import { Details } from './Details';
@@ -17,9 +18,17 @@ export default async function Dashboard() {
     <main className="relative mx-auto flex max-w-sm flex-col gap-6 px-6 pb-12">
       <Summary date={now} goal={goal} />
       <Details date={now} goal={goal} />
-      <Suspense>
+      <Suspense fallback={<Fallback />}>
         <Statistics now={now} />
       </Suspense>
     </main>
+  );
+}
+
+function Fallback() {
+  return (
+    <div className="flex items-center justify-center">
+      <Spinner />
+    </div>
   );
 }
